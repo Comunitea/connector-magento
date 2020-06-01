1ce11da64b0956475232604ab2a376e0c9c2087b
JesusVMayor <jesus@comunitea.com>
2020-06-01 12:45:29 +0200

    Evitamos exportación de facturas

--- magentoerpconnect/invoice.py
+++ magentoerpconnect/invoice.py
@@ -230,47 +230,47 @@ class MagentoInvoiceExporter(Exporter):
 MagentoInvoiceSynchronizer = MagentoInvoiceExporter  # deprecated
 
 
-@on_invoice_validated
-@on_invoice_paid
-def invoice_create_bindings(session, model_name, record_id):
-    """
-    Create a ``magento.account.invoice`` record. This record will then
-    be exported to Magento.
-    """
-    invoice = session.env[model_name].browse(record_id)
-    # find the magento store to retrieve the backend
-    # we use the shop as many sale orders can be related to an invoice
-    for sale in invoice.sale_ids:
-        for magento_sale in sale.magento_bind_ids:
-            binding_exists = False
-            for mag_inv in invoice.magento_bind_ids:
-                if mag_inv.backend_id.id == magento_sale.backend_id.id:
-                    binding_exists = True
-                    break
-            if binding_exists:
-                continue
-            # Check if invoice state matches configuration setting
-            # for when to export an invoice
-            magento_store = magento_sale.store_id
-            payment_method = sale.payment_method_id
-            if payment_method and payment_method.create_invoice_on:
-                create_invoice = payment_method.create_invoice_on
-            else:
-                create_invoice = magento_store.create_invoice_on
+# @on_invoice_validated
+# @on_invoice_paid
+# def invoice_create_bindings(session, model_name, record_id):
+#     """
+#     Create a ``magento.account.invoice`` record. This record will then
+#     be exported to Magento.
+#     """
+#     invoice = session.env[model_name].browse(record_id)
+#     # find the magento store to retrieve the backend
+#     # we use the shop as many sale orders can be related to an invoice
+#     for sale in invoice.sale_ids:
+#         for magento_sale in sale.magento_bind_ids:
+#             binding_exists = False
+#             for mag_inv in invoice.magento_bind_ids:
+#                 if mag_inv.backend_id.id == magento_sale.backend_id.id:
+#                     binding_exists = True
+#                     break
+#             if binding_exists:
+#                 continue
+#             # Check if invoice state matches configuration setting
+#             # for when to export an invoice
+#             magento_store = magento_sale.store_id
+#             payment_method = sale.payment_method_id
+#             if payment_method and payment_method.create_invoice_on:
+#                 create_invoice = payment_method.create_invoice_on
+#             else:
+#                 create_invoice = magento_store.create_invoice_on
 
-            if create_invoice == invoice.state:
-                session.env['magento.account.invoice'].create({
-                    'backend_id': magento_sale.backend_id.id,
-                    'openerp_id': invoice.id,
-                    'magento_order_id': magento_sale.id})
+#             if create_invoice == invoice.state:
+#                 session.env['magento.account.invoice'].create({
+#                     'backend_id': magento_sale.backend_id.id,
+#                     'openerp_id': invoice.id,
+#                     'magento_order_id': magento_sale.id})
 
 
-@on_record_create(model_names='magento.account.invoice')
-def delay_export_account_invoice(session, model_name, record_id, vals):
-    """
-    Delay the job to export the magento invoice.
-    """
-    export_invoice.delay(session, model_name, record_id)
+# @on_record_create(model_names='magento.account.invoice')
+# def delay_export_account_invoice(session, model_name, record_id, vals):
+#     """
+#     Delay the job to export the magento invoice.
+#     """
+#     export_invoice.delay(session, model_name, record_id)
 
 
 @job(default_channel='root.magento')
